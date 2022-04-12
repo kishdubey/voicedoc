219a4b5733a9b34e42677c555c67e0b804b12262
Kish Dubey <dkish2000@gmail.com>
2022-04-11 20:44:26 -0400

    formatting

--- application/voice/transcribe.py
+++ application/voice/transcribe.py
@@ -5,17 +5,18 @@ from application.config import config
 from glob import glob
 import random
 
+
 def transcribe(input_file):
     device = torch.device('cpu')
     model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
                                            model='silero_stt',
                                            jit_model='jit_xlarge',
-                                           language='en', 
+                                           language='en',
                                            device=device)
 
     (read_batch, split_into_batches,
      read_audio, prepare_model_input) = utils
-    
+
     test_files = glob(input_file)
     batches = split_into_batches(test_files, batch_size=10)
     batch = read_batch(random.sample(batches, k=1)[0])
@@ -32,5 +33,6 @@ def transcribe(input_file):
         transcript.append(decoder(word.cpu(), wav_len, word_align=True)[-1])
 
     return transcript
-    
+
+
 print(transcribe(config.SPEAKER_FILE)[0][0])
