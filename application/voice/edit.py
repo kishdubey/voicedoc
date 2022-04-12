b85b724f0a3c6ee611f8e3688fb41a80e7849917
Kish Dubey <dkish2000@gmail.com>
2022-04-11 20:44:40 -0400

    audio editing working

---
+++ application/voice/edit.py
@@ -0,0 +1,26 @@
+from pydub import AudioSegment
+
+
+def get_audio(filepath):
+    return AudioSegment.from_wav(filepath)
+
+
+def trim(audio, duration, before=True):
+    '''
+    boolean: before
+    first part of clipping unitl duration is returned if 'before' is True. 
+    second part of clipping after duration is returned if 'before' is False.
+    '''
+    return audio[:_to_milliseconds(duration)] if before else audio[_to_milliseconds(duration):]
+
+
+def concatenate(audio1, audio2):
+    return audio1 + audio2
+
+
+def _to_seconds(ms):
+    return ms/1000
+
+
+def _to_milliseconds(s):
+    return s*1000
