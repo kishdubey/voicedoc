d5b38001818319b37d628f62af33e3d2d733fd88
Kish Dubey <dkish2000@gmail.com>
2022-09-23 20:47:35 -0400

    file downloaded after audio exported

--- application/app.py
+++ application/app.py
@@ -1,7 +1,7 @@
 import os
 import ast
 
-from flask import Flask, redirect, render_template, request, flash, url_for
+from flask import Flask, current_app, redirect, render_template, request, flash, send_file, send_from_directory, url_for
 from werkzeug.utils import secure_filename
 
 from application.config.config import config
@@ -31,44 +31,46 @@ def edit():
 
         transcript = transcribe(config.transcribe_file)
     return render_template("edit.html", audio_file=config.speaker_file, transcript=transcript, transcript_words=to_words(transcript))
-    return redirect(url_for('index'))
 
 
-@app.route('/handle_data', methods=['POST'])
+@app.route('/handle_data', methods=['GET', 'POST'])
 def handle_data():
-    valid_words = request.form['validWords'].split(',')
-    unvalid_words = request.form['unvalidWords'].split(',')
-    transcript_words = ast.literal_eval(request.form['transcriptWords'])
-    transcript = ast.literal_eval(request.form['transcript'])
+    if request.method == 'POST':
+        valid_words = request.form['validWords'].split(',')
+        unvalid_words = request.form['unvalidWords'].split(',')
+        transcript_words = ast.literal_eval(request.form['transcriptWords'])
+        transcript = ast.literal_eval(request.form['transcript'])
 
-    # getting timestamps for unvalid words
-    unvalid_word_timestamps = []
-    for word in unvalid_words:
-        unvalid_word_timestamps.append(find_word(word, transcript))
-    unvalid_word_timestamps = list(filter(None, unvalid_word_timestamps))
+        # getting timestamps for unvalid words
+        unvalid_word_timestamps = []
+        for word in unvalid_words:
+            unvalid_word_timestamps.append(find_word(word, transcript))
+        unvalid_word_timestamps = list(filter(None, unvalid_word_timestamps))
 
-    for word in unvalid_word_timestamps:
-        time = word['end_ts'] - word['start_ts']
-        time_start = word['start_ts'] - time*2 if (word['start_ts'] - time*2) > 0 else 0
-        time_end = word['end_ts'] + time*2
+        for word in unvalid_word_timestamps:
+            time = word['end_ts'] - word['start_ts']
+            time_start = word['start_ts'] - time * \
+                2 if (word['start_ts'] - time*2) > 0 else 0
+            time_end = word['end_ts'] + time
 
-        # remove word from transcript audio from unvalid_word_timestamps
-        trim(config.transcribe_file, time_start, time_end)
+            # remove word from transcript audio from unvalid_word_timestamps
+            trim(config.transcribe_file, time_start, time_end)
 
-        # delete that word obj from unvalid_word_timestamps and transcript
-        delete_word(word, transcript)
-        delete_word(word, unvalid_word_timestamps)
+            # delete that word obj from unvalid_word_timestamps and transcript
+            delete_word(word, transcript)
+            delete_word(word, unvalid_word_timestamps)
 
-        # adjust_transcript transcript and unvalid_word_timestamps
-        adjust_transcript(time, transcript)
-        adjust_transcript(time, unvalid_word_timestamps)
+            # adjust_transcript transcript and unvalid_word_timestamps
+            adjust_transcript(time, transcript)
+            adjust_transcript(time, unvalid_word_timestamps)
 
-    # find word to add in betweeen two valid words
-    # syntheizes that word
-    # append in between end_ts and start_ts of words on left and right respectively
+        # find word to add in betweeen two valid words
+        # syntheizes that word
+        # append in between end_ts and start_ts of words on left and right respectively
 
-    return "Trimmed"
-
+    # sending file 
+    filename = config.transcribe_file.split('/')[-1]
+    return send_file(config.transcribe_file, download_name=filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
